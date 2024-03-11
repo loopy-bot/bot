70e2334c6de11e94f6989c4bbc388a104387fff1
huangyuan06 <huangyuan06@kuaishou.com>
2024-03-11 17:20:14 +0800

    feat: 连接数据库

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '123.60.1.214',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'wx_bot',
+      synchronize: true,
+      logging: true,
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      entities: [__dirname + '/**/entities/*.entity{.js,.ts}'],
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
