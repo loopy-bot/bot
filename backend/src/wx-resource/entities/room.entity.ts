6c09fba46cb14c31ad4e5d97160309c36e9cce8c
huangyuan06 <huangyuan06@kuaishou.com>
2024-03-12 21:09:32 +0800

    feat: 构建群聊和好友实体

--- backend/src/wx-resource/entities/room.entity.ts
+++ backend/src/wx-resource/entities/room.entity.ts
@@ -4,4 +4,10 @@ import { Column } from 'src/decorators/createColumn';
 export class Room extends BaseEntity {
   @Column({ nullable: true, isRequired: true })
   name: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @Column({ isRequired: true })
+  wx_id: string;
 }
