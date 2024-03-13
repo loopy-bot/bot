9aa1cb8ee0af147eb0c27a81f663578b982fc1b6
Jsweet <2171077189@qq.com>
2024-03-13 22:41:29 +0800

    feat: 整理gitignore

---
+++ wx-bot/config.js
@@ -0,0 +1,17 @@
+// 模型配置
+export const AI_MODAL = "qwen"; // 切换不同模型，目前只对阿里的支持多轮对话
+export const TIAN_KEY = "1f05c22e289b9e49281950e02e1bbece";
+export const OPENAI_KEY = "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";
+export const GEMINI_KEY = "AIzaSyBhRHTesPTVStnpQKIQA_FOm9VNdP3SpHk";
+
+// 邮箱配置
+export const mailConfig = {
+  auth: {
+    user: "nanmi1436604@163.com", // 网易邮箱账号
+    pass: "UCCIPKNISYHXDMNM", // 网易邮箱授权码
+  },
+  mailOptions: {
+    from: "nanmi1436604@163.com", // 发件人
+    to: "2171077189@qq.com", // 收件人列表，多个收件人用逗号分隔
+  },
+};
